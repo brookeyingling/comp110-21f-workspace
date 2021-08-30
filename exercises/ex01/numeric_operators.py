95f438640e2bc39f56db97ff0f74a15e436b2256
Brooke Yingling <brooke22@email.unc.edu>
2021-08-30 18:10:19 -0400

    math

---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,13 @@
+"""Exercise 1 part 2 numeric operators."""
+
+_author_ = "730405432"
+
+
+initialnumber1: str = input("Left-hand side:")
+initialnumber2: str = input("Right-hand side:")
+number1 = int(initialnumber1) 
+number2 = int(initialnumber2)
+print(str(number1) + " ** " + str(number2) + " is " + str(number1 ** number2))
+print(str(number1) + " / " + str(number2) + " is " + str(number1 / number2))
+print(str(number1) + " // " + str(number2) + " is " + str(number1 // number2))
+print(str(number1) + " % " + str(number2) + " is " + str(number1 % number2))
