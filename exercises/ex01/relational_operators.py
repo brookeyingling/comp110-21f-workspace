a5ecd81a797c8cfe8c4b627feb6d77061979f8c3
Brooke Yingling <brooke22@email.unc.edu>
2021-08-30 18:20:36 -0400

    used a similar structure from numeric to apply the same concept with true/false

---
+++ exercises/ex01/relational_operators.py
@@ -0,0 +1,14 @@
+"""Exercise 1 part 3."""
+
+_author_ = "730405432"
+
+
+initialnumber1: str = input("Left-hand side:")
+initialnumber2: str = input("Right-hand side:")
+number1 = int(initialnumber1) 
+number2 = int(initialnumber2)
+
+print(str(number1) + " < " + str(number2) + " is " + str(number1 < number2))
+print(str(number1) + " >= " + str(number2) + " is " + str(number1 >= number2))
+print(str(number1) + " == " + str(number2) + " is " + str(number1 == number2))
+print(str(number1) + " != " + str(number2) + " is " + str(number1 != number2))
