350b56b4b90c2215dacf7cae973e35d7cff66f7c
Brooke Yingling <brooke22@email.unc.edu>
2021-08-31 17:26:09 -0400

    final edits

--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,14 +1,13 @@
-"""Exercise 1 part 3."""
+"""Exercise 1 part 3 relational operators."""
 
 __author__ = "730405432"
 
-"""Applied concepts from part 2 to solve True/False equations."""
+"""Add strings together but subsitute the final string with an equation."""
 
 initialnumber1: str = input("Left-hand side:")
 initialnumber2: str = input("Right-hand side:")
 number1 = int(initialnumber1) 
 number2 = int(initialnumber2)
-
 print(str(number1) + " < " + str(number2) + " is " + str(number1 < number2))
 print(str(number1) + " >= " + str(number2) + " is " + str(number1 >= number2))
 print(str(number1) + " == " + str(number2) + " is " + str(number1 == number2))
