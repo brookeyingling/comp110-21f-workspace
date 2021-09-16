c9ba72486a51ebb9acf1427e7e4c6e02918f28ed
Brooke Yingling <brooke22@email.unc.edu>
2021-09-15 22:07:55 -0400

    final exercise updates

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,19 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730405432"
+word: str = input("Enter a word: ")
+i: int = 0
+duplicate: int = 0
+found: bool = False
+
+while i < len(word):
+    duplicate = i + 1
+    while duplicate < len(word):
+        if word[i] == word[duplicate]:
+            found = True
+            duplicate = duplicate + 1
+        else:
+            duplicate = duplicate + 1
+    i = i + 1
+
+print(("Found duplicate: ") + (str(found)))
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,13 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730405432"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+number: int = int(input("Depth: "))
+space: str = " "
+i: int = 0
+
+while i < number:
+    i = i + 1
+    print(TREE * i)
