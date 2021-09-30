e61e3ab48de100f65bfad1cea808e8935458027a
Brooke Yingling <brooke22@email.unc.edu>
2021-09-29 23:46:21 -0400

    exercise 4

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,47 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730405432"
 
 
-# TODO: Implement your functions here.
+def all(numbers: list[int], number: int) -> bool:
+    """Determine whether number is equal to the list numbers."""
+    i: int = 0
+    if len(numbers) == 0:
+        return False
+    else:
+        while i < len(numbers):
+            if numbers[i] != number:
+                return False
+            else:
+                i += 1
+        return True
+
+
+def is_equal(first_list: list[int], second_list: list[int]) -> bool:
+    """Determining whether or not two lists are equal."""
+    i: int = 0
+    b: int = 0
+    if len(first_list) != len(second_list):
+        return False
+    while i < len(first_list):
+        if first_list[i] != second_list[b]:
+            return False
+        else:
+            i += 1
+            b += 1
+    return True
+
+
+def max(input: list[int]) -> int:
+    """Determing what the maximum value is in a given list of integers."""
+    i: int = 0
+    b: int = 0
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty list")
+    while len(input) > i:
+        if input[i] > input[b]:
+            b += 1
+            return input[i]
+        else:
+            i += 1
+            return input[b]
